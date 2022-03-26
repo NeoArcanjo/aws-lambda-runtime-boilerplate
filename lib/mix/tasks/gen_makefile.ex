76df7c4e9991b9c7bfdc6a0cd88f5450b1d2dbc0
Rafael Arcanjo <rexmundi.rafael@gmail.com>
2022-03-26 15:24:31 -0300

    modified:   lib/mix/tasks/gen_makefile.ex

--- lib/mix/tasks/gen_makefile.ex
+++ lib/mix/tasks/gen_makefile.ex
@@ -48,21 +48,21 @@ defmodule Mix.Tasks.Aws.Gen.Makefile do
 	build: .make/build
 
 	run: .make/build
-		docker run -it erlambda $(CMD)
+	\tdocker run -it erlambda $(CMD)
 
 	bind: .make/base
-		docker run -it --mount type=bind,source=$(shell pwd),target=/bind elixirbase sh -ec "cd /bind/$(WD); $(CMD)"
+	\tdocker run -it --mount type=bind,source=$(shell pwd),target=/bind elixirbase sh -ec "cd /bind/$(WD); $(CMD)"
 
 	.make/build: .make/base Dockerfile $(PROJECTFILES)
-		docker build -t erlambda .
-		date > .make/build
+	\tdocker build -t erlambda .
+	\tdate > .make/build
 
 	.make/base: .make images/base.Dockerfile
-		docker build -t elixirbase -f $(shell pwd)/images/base.Dockerfile .
-		date > .make/base
+	\tdocker build -t elixirbase -f $(shell pwd)/images/base.Dockerfile .
+	\tdate > .make/base
 
 	.make:
-		mkdir -p .make
+	\tmkdir -p .make
 	"""
   end
 end
