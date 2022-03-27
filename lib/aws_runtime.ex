e101d85df7aee886d4e16479b12b5fda0a9047b9
Rafael Arcanjo <rexmundi.rafael@gmail.com>
2022-03-27 11:54:45 -0300

    modified:   lib/aws_runtime.ex 	modified:   lib/mix/tasks/gen_makefile.ex 	new file:   lib/templates/Dockerfile.eex 	renamed:    Makefile -> lib/templates/Makefile 	new file:   lib/templates/base.Dockerfile.eex

--- lib/aws_runtime.ex
+++ lib/aws_runtime.ex
@@ -2,17 +2,4 @@ defmodule AwsRuntime do
   @moduledoc """
   Documentation for `AwsRuntime`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AwsRuntime.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- lib/mix/tasks/gen_makefile.ex
+++ lib/mix/tasks/gen_makefile.ex
@@ -4,6 +4,38 @@ defmodule Mix.Tasks.Aws.Gen.Makefile do
   """
 
   use Mix.Task
+  import Mix.Generator
+
+  embed_text(
+    :makefile,
+    """
+    CMD?=
+    WD?=
+
+    PROJECTFILES=$(shell ls -1 *.ex{s,}) $(shell find lib src test -type f)
+    .PHONY: run base bind
+
+    base: .make/base
+    build: .make/build
+
+    run: .make/build
+    \tdocker run -it erlambda $(CMD)
+
+    bind: .make/base
+    \tdocker run -it --mount type=bind,source=$(shell pwd),target=/bind elixirbase sh -ec "cd /bind/$(WD); $(CMD)"
+
+    .make/build: .make/base Dockerfile $(PROJECTFILES)
+    \tdocker build -t erlambda .
+    \tdate > .make/build
+
+    .make/base: .make images/base.Dockerfile
+    \tdocker build -t elixirbase -f $(shell pwd)/images/base.Dockerfile .
+    \tdate > .make/base
+
+    .make:
+    \tmkdir -p .make
+    """
+  )
 
   @shortdoc "Generate a Makefile for the project"
 
@@ -16,7 +48,6 @@ defmodule Mix.Tasks.Aws.Gen.Makefile do
   end
 
   def run(_args) do
-    # todo: obter do cli a elixir version e erlang version
     # name =
     #   Mix.Project.config()
     #   |> Keyword.fetch!(:app)
@@ -30,39 +61,41 @@ defmodule Mix.Tasks.Aws.Gen.Makefile do
     #     strict: []
     #   )
 
+    Mix.Generator.copy_template(makefile_text(), "Makefile.1", [])
+
     Mix.Generator.create_file(
       "Makefile",
-      makefile_template()
+      makefile_text()
     )
   end
 
-  defp makefile_template do
-	"""
-	CMD?=
-	WD?=
+#   defp makefile_template do
+#     """
+#     CMD?=
+#     WD?=
 
-	PROJECTFILES=$(shell ls -1 *.ex{s,}) $(shell find lib src test -type f)
-	.PHONY: run base bind
+#     PROJECTFILES=$(shell ls -1 *.ex{s,}) $(shell find lib src test -type f)
+#     .PHONY: run base bind
 
-	base: .make/base
-	build: .make/build
+#     base: .make/base
+#     build: .make/build
 
-	run: .make/build
-	\tdocker run -it erlambda $(CMD)
+#     run: .make/build
+#     \tdocker run -it erlambda $(CMD)
 
-	bind: .make/base
-	\tdocker run -it --mount type=bind,source=$(shell pwd),target=/bind elixirbase sh -ec "cd /bind/$(WD); $(CMD)"
+#     bind: .make/base
+#     \tdocker run -it --mount type=bind,source=$(shell pwd),target=/bind elixirbase sh -ec "cd /bind/$(WD); $(CMD)"
 
-	.make/build: .make/base Dockerfile $(PROJECTFILES)
-	\tdocker build -t erlambda .
-	\tdate > .make/build
+#     .make/build: .make/base Dockerfile $(PROJECTFILES)
+#     \tdocker build -t erlambda .
+#     \tdate > .make/build
 
-	.make/base: .make images/base.Dockerfile
-	\tdocker build -t elixirbase -f $(shell pwd)/images/base.Dockerfile .
-	\tdate > .make/base
+#     .make/base: .make images/base.Dockerfile
+#     \tdocker build -t elixirbase -f $(shell pwd)/images/base.Dockerfile .
+#     \tdate > .make/base
 
-	.make:
-	\tmkdir -p .make
-	"""
-  end
+#     .make:
+#     \tmkdir -p .make
+#     """
+#   end
 end
