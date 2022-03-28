6f0c5e619ccd0f8bb9282f7540521353efd2fd83
Rafael Arcanjo <rexmundi.rafael@gmail.com>
2022-03-28 12:02:59 -0300

    modified:   .gitignore

--- testando/images/cache.Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM elixirbase:latest
-
-ENV MIX_ENV=dev
-
-COPY . /testando
-WORKDIR /testando
-
-RUN mix deps.get
-RUN mix compile
--- testando/test/testando_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TestandoTest do
-  use ExUnit.Case
-  doctest Testando
-
-  test "greets the world" do
-    assert Testando.hello() == :world
-  end
-end
